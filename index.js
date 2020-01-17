fa2b36d735061e123f65de4cc9258fabc64b9edc
임동완 <limson_95@naver.com>
2020-01-17 17:10:36 +0900

    account indexing

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const api = require('./routes');
+
+app.use('/api', api);
 app.get('/', (req, res) => {
   res.send('real-crowd');
 });
