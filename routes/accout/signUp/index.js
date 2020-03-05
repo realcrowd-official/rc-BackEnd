91ab133cea3eb09fb84d89b19289e692119c7064
임동완 <limson_95@naver.com>
2020-03-05 19:51:21 +0900

    [ADD] user img 업로드

--- routes/accout/signUp/index.js
+++ routes/accout/signUp/index.js
@@ -1,23 +1,33 @@
 const router = require('express').Router();
+const multer = require('multer');
+const multerGoogleStorage = require('multer-google-storage');
 
 const authMiddleWare = require('../../../lib/resolveJwt').signUpAuthMiddleware;
 const { signInJwt } = require('../../../lib/encodeJwt');
 
 const { signUp, checkNickName } = require('../../../db/query/account');
 
+const uploadOption = multer({
+  storage: multerGoogleStorage.storageEngine({
+    bucket: process.env.googleStorageBucket,
+    projectId: process.env.googleStorageId,
+    keyFilename: process.env.googleKeyFile
+  })
+  // limits: { fileSize: 10 * 1024 * 1024 }
+});
+
 router.use('/', authMiddleWare);
 
-router.post('/', async (req, res) => {
+router.post('/', uploadOption.single('img'), async (req, res) => {
   const query = {
     id: req.decoded.id,
     nickname: req.body.nickname,
     name: req.body.name,
     phone: req.body.phone,
     email: req.decoded.email ? req.decoded.email : '',
-    social: req.decoded.social
+    social: req.decoded.social,
+    thumNialPic: req.file.path
   };
-
-  // eslint-disable-next-line no-unused-expressions
   if (await checkNickName(req.body.nickname)) {
     res.status(200).json({ statusCode: 409, ans: 'nickname' });
   }
